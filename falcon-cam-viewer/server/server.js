52c5d24e12563ab09e3dc525834ea4cd8eba312a
sherryyar <syar@deloitte.com.au>
2025-06-04 11:27:51 +1000

    Initial commit

--- falcon-cam-viewer/server/server.js
+++ falcon-cam-viewer/server/server.js
@@ -6,6 +6,7 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const compression = require('compression');
 const winston = require('winston');
+const path = require('path');
 
 // Load environment variables
 dotenv.config();
@@ -212,6 +213,14 @@ app.get('/api/opsgenie/schedules/:scheduleId/on-calls', async (req, res, next) =
   } catch (error) {
     next(error);
   }
+});
+
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, '../build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 // Apply error handling middleware
